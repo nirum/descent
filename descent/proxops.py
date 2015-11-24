1a2afac3c42cf8472aeb201e7da87d0c09889937
Niru Maheswaranathan <nirum@stanford.edu>
2015-11-24 18:08:39 -0500

    adds projection onto the probability simplex

--- descent/proxops.py
+++ descent/proxops.py
@@ -166,6 +166,22 @@ def linear(x, rho, weights):
 
 
 @proxify
+def simplex(x, rho):
+    """
+    Projection onto the probability simplex
+
+    http://arxiv.org/pdf/1309.1541v1.pdf
+
+    """
+
+    # sort the elements in descending order
+    u = np.flipud(np.sort(x.ravel()))
+    lambdas = (1 - np.cumsum(u)) / (1. + np.arange(u.size))
+    ix = np.where(u + lambdas > 0)[0].max()
+    return np.maximum(x + lambdas[ix], 0)
+
+
+@proxify
 def fantope(x, rho, dim, tol=1e-4):
     """
     Projection onto the fantope
