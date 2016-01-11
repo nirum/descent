847d1f37424a4fe415595cf59fcd922e9690c672
Niru Maheswaranathan <nirum@stanford.edu>
2016-01-10 19:24:16 -0800

    adds the ability to modify the shape before applying the smoothing proximal operator

--- descent/proxops.py
+++ descent/proxops.py
@@ -185,18 +185,36 @@ def nonneg(x, rho):
 
 
 @proxify
-def smooth(x, rho, penalty, axis=0):
+def smooth(x, rho, penalty, axis=0, newshape=None):
     """
     Applies a smoothing operator along one dimension
 
     currently only accepts a matrix as input
+
+    Parameters
+    ----------
+    penalty : float
+
+    axis : int, optional
+        Axis along which to apply the smoothing (Default: 0)
+
+    newshape : tuple, optional
+        Desired shape of the parameters to apply the nuclear norm to. The given
+        parameters are reshaped to an array with this shape, or not reshaped if
+        the value of newshape is None. (Default: None)
+
     """
+
+    orig_shape = x.shape
+
+    if newshape is not None:
+        x = x.reshape(newshape)
 
     # Apply Laplacian smoothing (l2 norm on the parameters multiplied by
     # the laplacian)
     n = x.shape[axis]
     lap_op = spdiags([(2 + rho / penalty) * np.ones(n), -1 * np.ones(n), -1 * np.ones(n)], [0, -1, 1], n, n, format='csc')
-    return np.rollaxis(spsolve(penalty * lap_op, rho * np.rollaxis(x, axis, 0)), axis, 0)
+    return np.rollaxis(spsolve(penalty * lap_op, rho * np.rollaxis(x, axis, 0)), axis, 0).reshape(orig_shape)
 
 
 @proxify
