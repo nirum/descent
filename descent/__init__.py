186fa162bde95ef83bdd926e0ff4ddc58dff78dc
Niru Maheswaranathan <nirum@stanford.edu>
2016-10-07 14:12:12 -0700

    Adds objectives to __init__

--- descent/__init__.py
+++ descent/__init__.py
@@ -8,12 +8,14 @@ A Python package for performing first-order optimization
 __version__ = '0.2.0'
 
 from .algorithms import *
+from .objectives import *
 from .proxops import *
 from .utils import *
 from .main import *
 
 __all__ = [
     *algorithms.__all__,
+    *objectives.__all__,
     *proxops.__all__,
     *utils.__all__,
     *main.__all__,
