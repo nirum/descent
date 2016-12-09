e95d01ee4adff88083444dc2c21c6639ee628993
Niru Maheswaranathan <nirum@stanford.edu>
2016-12-09 18:47:53 +0100

    Working on regularized minimum polynomial expansion

---
+++ descent/rmpe.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+__all__ = ['rmpe']
+
+
+def getU(xs):
+    """xs is an (n x k) matrix"""
+    return np.diff(xs, axis=1)
+
+
+def rmpe(U, lmbda=1e-5):
+    A = U.T @ U + lmbda * np.eye(U.shape[1],)
+    z = np.linalg.solve(A, np.ones(U.shape[1],))
+    return z / z.sum()
+
+
+def xhat(xs):
+    U = getU(xs)
+    z = rmpe(U, lmbda=1e-5)
+    return xs[:, :-1] @ z
