074c8452f15a0da638668a4fe139fde06ccfae7f
Niru Maheswaranathan <nirum@stanford.edu>
2017-06-29 21:47:15 -0700

    minor :bug:fix

--- descent/objectives.py
+++ descent/objectives.py
@@ -40,14 +40,14 @@ def objective(param_scales=(1, 1), xstar=None, seed=None):
     return decorator
 
 
-@objective(xstar=(0,), param_scales=(10,))
+@objective(xstar=0, param_scales=(10,))
 def doublewell(theta):
     """Pointwise minimum of two quadratic bowls"""
     k0, k1, depth = 0.01, 100, 0.5
     shallow = 0.5 * k0 * theta ** 2 + depth
     deep = 0.5 * k1 * theta ** 2
-    obj = np.minimum(shallow, deep)
-    grad = np.where(deep < shallow, k0 * theta, k1 * theta)
+    obj = float(np.minimum(shallow, deep))
+    grad = np.where(deep < shallow, k1 * theta, k0 * theta)
     return obj, grad
 
 
